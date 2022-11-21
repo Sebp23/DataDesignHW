2b83a82f9a9d64bb058cc023c4950a182eb05935
Sebastian Pedersen <57201896+Sebp23@users.noreply.github.com>
2022-11-20 20:45:07 -0600

    JSON for Forms Solution

---
+++ Week 11/HW/Models/ChartThreeModel.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Week11_HW_Forms.Models
+{
+    public class ChartThreeModel
+    {
+        [JsonProperty("HP")]
+        public int? HP { get; set; }
+
+        [JsonProperty("MP")]
+        public int? MP { get; set; }
+    }
+}
