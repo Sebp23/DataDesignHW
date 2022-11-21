a5b7edfbe44d9d5119cf3ca4d7eb8f209aecd7db
Sebastian Pedersen <57201896+Sebp23@users.noreply.github.com>
2022-11-20 20:54:13 -0600

    Re-added week 11

---
+++ Week 11/HW/Week11_HW_Forms/Services/Errors.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week11_HW_Forms.Services
+{
+    internal class Errors
+    {
+        public string Message { get; set; }
+        public string MethodSource { get; set; }
+        public static List<Errors> ErrorList = new List<Errors>();
+
+        public Errors(string message, string methodSource)
+        {
+            this.Message = message;
+            this.MethodSource = methodSource;
+        }
+    }
+}
